82f1101f7cdaa87219c0d95512f46febbb1d6d1b
Jean Paul <jeanpaul756@gmail.com>
2018-01-29 11:15:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com This was change by me\n");
+  response.end("Hello this was change by me. Thanks getintodevops.com and thepolyglotdeveloper.com\n");
 });
 
 // listen on localhost:8000
